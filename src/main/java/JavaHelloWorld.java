0dafc266cb655b6756bda29069edda8d4aa432a8
Wruthless <wsgnzls@comcast.net>
2023-05-17 04:29:50 -0400

    Initial commit.

---
+++ src/main/java/JavaHelloWorld.java
@@ -0,0 +1,3 @@
+public class JavaHelloWorld {
+    public String getHello() {return "Hello world.";}
+}
---
+++ src/test/java/JavaHelloWorldTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldTest {
+
+    @Test
+    void getHello() {
+        System.out.println(new JavaHelloWorld().getHello());
+    }
+}
+
